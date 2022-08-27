585c73cda8eb3da2a3ca01ecc9a20b4d50305ea9
Rahul Yadav <r.yadav9969@gmail.com>
2022-08-27 13:57:39 +0530

    initialize arcana

--- src/app/components/common/ConnectWallet/ConnectWallet.tsx
+++ src/app/components/common/ConnectWallet/ConnectWallet.tsx
@@ -6,33 +6,24 @@ import s from "./ConnectWallet.module.scss";
 const ConnectWallet: FC = () => {
   const [user, setUser] = useState({ loggedIn: false });
 
-  const initWallet = async () => {
-    const auth = new AuthProvider(`${AppConfig.ARCANA_APP_ID}`);
-    const position = "left"; // values - 'left' or 'right'
-    await auth.init({ appMode: AppMode.Widget, position });
-    // provider = auth.provider;
-    // const provider = await getWalletInstance();
-    // provider.on("connect", async () => {
-    //   const sp = await getStorageProvider(provider);
-    //   await sp.login();
-    //   setUser({ loggedIn: true });
-    // });
-    setTimeout(() => {}, 5000);
+  const authInit = async () => {
+    const auth = new AuthProvider(AppConfig.ARCANA_APP_ID!!);
+
+    await auth.init({
+      appMode: 2,
+      position: "right",
+    });
+
+    // await auth.loginWithSocial("google");
   };
 
   useEffect(() => {
-    initWallet();
+    authInit();
   }, []);
+
   return (
     <>
-      <div
-        className={s.connectWallet}
-        onClick={() => {
-          initWallet();
-        }}
-      >
-        Connect Wallet
-      </div>
+      <div className={s.connectWallet}>Connect Wallet</div>
     </>
   );
 };
